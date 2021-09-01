4305293b59bed5eebfbc69ff7c93ba82de109883
minuk926 <minuk926@gmail.com>
2021-09-01 23:50:27 +0900

    prototype-shop

--- prototype-shop/src/components/Prototypes.jsx
+++ prototype-shop/src/components/Prototypes.jsx
@@ -1,3 +1,51 @@
+const data = [
+	{
+		id: 'pp-01',
+		title: 'Voice Note',
+		artist: 'Haerin Song',
+		desc: 'Made by Haerin Song(Soda Design)',
+		thumbnail: '',
+		price: 90,
+		pieUrl: 'https://cloud.protopie.io/p/7a0d6567d2'
+	},
+	{
+		id: 'pp-02',
+		title: 'Voice Note',
+		artist: 'Haerin Song',
+		desc: 'Made by Haerin Song(Soda Design)',
+		thumbnail: '',
+		price: 90,
+		pieUrl: 'https://cloud.protopie.io/p/7a0d6567d2'
+	},
+	{
+		id: 'pp-03',
+		title: 'Voice Note',
+		artist: 'Haerin Song',
+		desc: 'Made by Haerin Song(Soda Design)',
+		thumbnail: '',
+		price: 90,
+		pieUrl: 'https://cloud.protopie.io/p/7a0d6567d2'
+	},
+	{
+		id: 'pp-04',
+		title: 'Voice Note',
+		artist: 'Haerin Song',
+		desc: 'Made by Haerin Song(Soda Design)',
+		thumbnail: '',
+		price: 90,
+		pieUrl: 'https://cloud.protopie.io/p/7a0d6567d2'
+	},
+	{
+		id: 'pp-05',
+		title: 'Voice Note',
+		artist: 'Haerin Song',
+		desc: 'Made by Haerin Song(Soda Design)',
+		thumbnail: '',
+		price: 90,
+		pieUrl: 'https://cloud.protopie.io/p/7a0d6567d2'
+	}
+];
+
 export default function ProtoTypes(){
 	return(
 		<main>
